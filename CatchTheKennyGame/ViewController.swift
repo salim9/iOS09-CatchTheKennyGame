725999f95fe907006e81605f5471d30bb15d6b7a
salim9 <salimaniatv@gmail.com>
2021-02-02 00:20:05 +0300

    Initial Commit

---
+++ CatchTheKennyGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatchTheKennyGame
+//
+//  Created by Salim Uzun on 2.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
